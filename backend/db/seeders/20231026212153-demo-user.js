602dfef3a18570f3e5c186789eb668cd778bfdad
DrewKennedy <kennedycdrew@gmail.com>
2023-10-29 23:57:15 -0400

    Remove id from seeders to test model validations

--- backend/db/seeders/20231026212153-demo-user.js
+++ backend/db/seeders/20231026212153-demo-user.js
@@ -12,23 +12,20 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     options.validate = "true"
     await User.bulkCreate([
-      { id: 1,
-        email: 'demo@user.io',
+      {email: 'demo@user.io',
         username: 'Demo-lition',
         firstName: 'Demo',
         lastName: 'Lition',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
-        id: 2,
         email: 'user1@user.io',
         username: 'FakeUser1',
         firstName: 'Fake',
         lastName: 'User',
         hashedPassword: bcrypt.hashSync('password2')
       },
-      { id: 3,
-        email: 'user2@user.io',
+      { email: 'user2@user.io',
         username: 'FakeUser2',
         firstName: 'Brake',
         lastName: "Fuser",
