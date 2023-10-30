09b64c3940176004782b63a4bcd8fd8daa2d51b8
DrewKennedy <kennedycdrew@gmail.com>
2023-10-30 16:38:13 -0400

    Add seed information for one photo for each Spot

--- backend/db/seeders/20231030200047-demo-spotimages.js
+++ backend/db/seeders/20231030200047-demo-spotimages.js
@@ -21,11 +21,30 @@ module.exports = {
     options.validate = "true";
     await SpotImage.bulkCreate([
       { spotId: 1,
-        url: "",
+        url: "https://d27p2a3djqwgnt.cloudfront.net/wp-content/uploads/2016/03/16132917/farm-scene.jpg",
+        preview: true
+      },
+      { spotId: 2,
+        url : "https://static.wikia.nocookie.net/nicos-nextbots/images/c/c4/ParkingLotOfEternalEmptiness.png/revision/latest?cb=20220728202500",
+        preview: true
+      },
+      { spotId: 3,
+        url: "https://www.excelhighschool.com/wp-content/uploads/2016/08/excel_about.jpg",
+        preview: true
+      },
+      { spotId: 4,
+        url: "https://cdn.britannica.com/95/143895-050-FA4BAC47/Billboards-Broadway-New-York-City-Times-Square.jpg",
+        preview: true
+      },
+      { spotId: 5,
+        url: "https://stayva.s3.amazonaws.com/2019/10/22/manassasbattlefieldpark.jpg",
+        preview: true
+      },
+      { spotId: 6,
+        url: "https://cdn.80.lv/api/upload/content/c1/images/63f4a62b58c7e/widen_1840x0.jpeg",
         preview: true
       }
     ], options)
-
   },
 
   async down (queryInterface, Sequelize) {
@@ -35,5 +54,10 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
+    options.tableName = "SpotImages";
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      id: { [Op.in] : [1, 2, 3, 4, 5, 6]}
+    })
   }
 };
