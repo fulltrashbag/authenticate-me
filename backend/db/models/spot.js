287690a764733c7c183217314094ea87a7715066
DrewKennedy <kennedycdrew@gmail.com>
2023-10-31 00:23:56 -0400

    Add model/migration, validations/constraints for Booking.

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -26,6 +26,12 @@ module.exports = (sequelize, DataTypes) => {
         onDelete: 'CASCADE',
         hooks: true
       })
+
+      Spot.hasMany(models.Booking, {
+        foreignKey: 'spotId',
+        onDelete: 'CASCADE',
+        hooks: true
+      })
     }
   }
   Spot.init({
