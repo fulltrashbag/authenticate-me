5a594eac3cd1caddfa044a7a61c42008dd11d3a4
DrewKennedy <kennedycdrew@gmail.com>
2023-11-04 21:14:23 -0400

    Format session invalid credentials error

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -37,8 +37,7 @@ router.post('/', validateLogin, async (req, res, next) => {
   if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
     const err = new Error('Login failed');
     err.status = 401;
-    err.title = 'Login failed';
-    err.errors = { credential: 'The provided credentials were invalid.' };
+    err.message = 'Invalid credentials'
     return next(err);
   }
 
