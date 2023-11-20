849a9326254dae3d7143fcdee1eb722ef62d5c37
DrewKennedy <kennedycdrew@gmail.com>
2023-11-20 16:28:03 -0500

    Add login/logout/signup functionality, login/signup components

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -87,9 +87,9 @@ router.delete('/review-images/:imageid', requireAuth, async (req, res, next) => 
 
 
 // !API Test Route
-router.post('/test', function (req, res) {
-  res.json({ requestBody: req.body });
-});
+// router.post('/test', function (req, res) {
+//   res.json({ requestBody: req.body });
+// });
 
 module.exports = router
 
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -10,6 +10,30 @@ const apiRouter = require('./api');
 
 router.use('/api', apiRouter)
 
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
+  // Serve the frontend's index.html file at the root route
+  router.get('/', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
+    );
+  });
+
+  // Serve the static assets in the frontend's build folder
+  router.use(express.static(path.resolve("../frontend/build")));
+
+  // Serve the frontend's index.html file at all other routes NOT starting with /api
+  router.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+    );
+  });
+}
+
+
+
 // *Dev CSRF token reset
 router.get("/api/csrf/restore", (req, res) => {
   const csrfToken = req.csrfToken();
@@ -19,5 +43,11 @@ router.get("/api/csrf/restore", (req, res) => {
   });
 });
 
+if (process.env.NODE_ENV !== 'production') {
+  router.get('/api/csrf/restore', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.json({});
+  });
+}
 
 module.exports = router;
